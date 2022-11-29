7b0dc5e95b2b7ef772d61e7483e1a84fa21c7822
Shahito <paturange.duncan@gmail.com>
2022-11-29 12:47:33 +0000

    first commit

---
+++ pages/add-notes.php
@@ -0,0 +1,76 @@
+<?php
+require_once "../tools/tools.php";
+session_start();
+if(!is_login()) header("Location: ".ROOT."/pages/login");
+$conn=get_db_conn();
+if(!has_link_in_db($conn,$_SESSION['user_id'])) header("Location: ".ROOT."/pages/link-waiting");
+mysqli_close($conn);
+?>
+<html lang='fr'>
+    <head>
+        <title><?php echo SITE_NAME;?> - Nouveau mot</title>
+        <link rel="stylesheet" href="../style/main.css"/>
+        <link href="../ressources/tab-icon.svg" rel="icon"/>
+        <meta name="theme-color" content="#333">
+        <meta name="msapplication-navbutton-color" content="#333">
+        <meta name="apple-mobile-web-app-status-bar-style" content="#333">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Font awesome import -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body draggable="false" class="preload" style="overscroll-behavior-y: contain;">
+        <header>
+            <div id="bubble-action-home" class="bubble-action" onClick="goWithTransition('../',0)">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" alt="Icone de retour au menu">
+                    <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+                </svg>
+            </div>
+        </header>
+        <main>
+            <div id="new-note-container">
+                <div id="canvas-command">
+                    <span id="cancel-btn" class="btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" alt="Icone de croix, annuler">
+                            <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+                        </svg>
+                    </span>
+                    <div id="brush-size-selector">
+                        <span id="brush-size-1" size="2" selected><span class="icon-round-brush"></span></span>
+                        <span id="brush-size-2" size="10"><span class="icon-round-brush"></span></span>
+                        <span id="brush-size-3" size="20"><span class="icon-round-brush"></span></span>
+                        <span id="brush-size-4" size="100"><span class="icon-round-brush"></span></span>
+                    </div>
+                    <span id="save-btn" class="btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" alt="Icone de coche, valider">
+                            <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7 425.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
+                        </svg>
+                    </span>
+                </div>
+                <canvas id="draw-box"></canvas>
+                <div id="draw-palette">
+                    <span id="eraser-brush" color="eraser"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M290.7 57.4L57.4 290.7c-25 25-25 65.5 0 90.5l80 80c12 12 28.3 18.7 45.3 18.7H288h9.4H512c17.7 0 32-14.3 32-32s-14.3-32-32-32H387.9L518.6 285.3c25-25 25-65.5 0-90.5L381.3 57.4c-25-25-65.5-25-90.5 0zM297.4 416H288l-105.4 0-80-80L227.3 211.3 364.7 348.7 297.4 416z"/></svg></span>
+                    <span id="black-brush" color="#000" selected></span>
+                    <span id="red-brush" color="#f00"></span>
+                    <span id="pink-brush" color="#fe80c9"></span>
+                    <span id="green-brush" color="#0f0"></span>
+                    <span id="cyan-brush" color="#0ff"></span>
+                    <span id="blue-brush" color="#00f"></span>
+                    <span id="rainbow-brush" color="rnb"></span>
+                </div>
+            </div>
+            <form id="form-post-notes" action="../tools/add-notes" method="POST">
+                <input type="hidden" id="data-img" name="data-img" value=""/>
+            </form>
+        </main>
+        <div id="transition-bg"></div>
+        <footer></footer>
+    </body>
+    <script src="../tools/draw.js"></script>
+    <script src="../tools/page-transition.js"></script>
+    <script>
+        /* Prevent animation on load */
+        setTimeout(function() {
+            document.body.className="";
+        },500);
+    </script>
+</html>
---
+++ tools/draw.js
@@ -0,0 +1,115 @@
+var onPhone=window.innerWidth<=720;
+var brushSize=onPhone?1.4:2;
+var i=0;
+var brushColor="#000";
+var fillColor="#f3e779";
+var canvas=document.getElementById("draw-box");
+var cursorOffsetX=document.querySelector("div#new-note-container").offsetWidth/2-canvas.offsetWidth/2;
+var cursorOffsetY=document.querySelector("div#new-note-container").offsetHeight/2-canvas.offsetHeight/2;
+var ctx=canvas.getContext('2d');
+var cancel_btn=document.getElementById("cancel-btn");
+var save_btn=document.getElementById("save-btn");
+resize();
+ctx.fillStyle=fillColor;
+ctx.fillRect(0,0,canvas.offsetWidth,canvas.offsetHeight);
+var pos={x:0,y:0};
+setBrushColor();
+setBrushSize();
+cancel_btn.addEventListener("click",clear);
+save_btn.addEventListener("click",save);
+canvas.addEventListener('mousedown',setPosition);
+canvas.addEventListener('mouseenter',setPosition);
+canvas.addEventListener('touchstart',setPosition);
+canvas.addEventListener('mousedown',draw);
+canvas.addEventListener('mouseenter',draw);
+canvas.addEventListener('touchstart',draw);
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('touchmove',draw);
+function setPosition(e) {
+    if(e.targetTouches) {
+        let touch=e.targetTouches[0];
+        pos.x=touch.pageX;
+        pos.y=touch.pageY;
+    } else {
+        pos.x=e.clientX;
+        pos.y=e.clientY;
+    }
+    pos.x=pos.x-cursorOffsetX;
+    pos.y=pos.y-cursorOffsetY;
+}
+function resize() {
+    ctx.canvas.width=canvas.offsetWidth;
+    ctx.canvas.height=canvas.offsetHeight;
+}
+function draw(e) {
+    if((e.buttons==1 || e.type=="touchmove") && true) {
+        ctx.beginPath();
+        ctx.lineWidth=brushSize;
+        ctx.lineCap='round';
+        if(typeof brushColor!="string") {
+            ctx.strokeStyle=brushColor[Math.floor(i)];
+            if(i>brushColor.length) { i=0; } else { i++; }
+        } else {
+            ctx.strokeStyle=brushColor;
+        }
+        ctx.moveTo(pos.x,pos.y);
+        setPosition(e);
+        ctx.lineTo(pos.x,pos.y);
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+function setBrushColor() {
+    let brushSelectors=document.querySelectorAll("#draw-palette span");
+    brushSelectors.forEach(brush => {
+        brush.addEventListener("click",() => {
+            brushSelectors.forEach((el) => { el.removeAttribute("selected"); });
+            brush.setAttribute("selected","");
+            color=brush.getAttribute("color");
+            if(color==="rnb") {
+                brushColor=Array(
+                    "#f00","#f10","#f20","#f30","#f40","#f50","#f60","#f70",
+                    "#f80","#f90","#fa0","#fb0","#fc0","#fd0","#fe0",
+                    "#ff0","#ef0","#df0","#cf0","#bf0","#af0","#9f0","#8f0",
+                    "#7f0","#6f0","#5f0","#4f0","#3f0","#2f0","#1f0",
+                    "#0f0","#0f1","#0f2","#0f3","#0f4","#0f5","#0f6","#0f7",
+                    "#0f8","#0f9","#0fa","#0fb","#0fc","#0fd","#0fe",
+                    "#0ff","#0ef","#0df","#0cf","#0bf","#0af","#09f","#08f",
+                    "#07f","#06f","#05f","#04f","#03f","#02f","#01f",
+                    "#00f","#10f","#20f","#30f","#40f","#50f","#60f","#70f",
+                    "#80f","#90f","#a0f","#b0f","#c0f","#d0f","#e0f",
+                    "#f0f","#f0e","#f0d","#f0c","#f0b","#f0a","#f09","#f08",
+                    "#f07","#f06","#f05","#f04","#f03","#f02","#f01"
+                );
+            } else if(color==="eraser") {
+                brushColor=fillColor;
+            } else {
+                brushColor=color;
+            }
+        });
+    });
+}
+function setBrushSize() {
+    let brushSelectors=document.querySelectorAll("#canvas-command span:not(.btn)");
+    brushSelectors.forEach(brush => {
+        brush.addEventListener("click",() => {
+            brushSelectors.forEach((el) => { el.removeAttribute("selected"); });
+            brushSize=onPhone?brush.getAttribute("size")*0.7:brush.getAttribute("size");
+            brush.setAttribute("selected","");
+        });
+    });
+}
+function clear() {
+    ctx.fillStyle="#f3e779";
+    ctx.fillRect(0,0,canvas.offsetWidth,canvas.offsetHeight);
+}
+function save(e) {
+    let dataURL=canvas.toDataURL();
+    let form=document.getElementById("form-post-notes");
+    let data_input=document.getElementById("data-img");
+    e.preventDefault();
+    let transition_bg=document.getElementById("transition-bg");
+    transition_bg.setAttribute("active-bg0","true");
+    data_input.value=dataURL;
+    form.submit();
+}
