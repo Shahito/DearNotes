4c1b47d64bb8ddffaa416e3491caa76c430e693d
Shahito <paturange.duncan@gmail.com>
2025-12-07 14:33:16 +0100

    V2 - total rework / node migration

--- tools/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "./tools.php";
-header("Location: ".ROOT."/pages/login");
-session_start();
-session_unset();
-session_destroy();
-setcookie("AUTH_TOKEN",null,-1,"/"); 
-?>
--- tools/real_screen_height.js
+++
@@ -1,6 +0,0 @@
-const documentHeight = () => {
-    const doc=document.documentElement
-    doc.style.setProperty('--real-vh', `${window.innerHeight}px`)
-}
-window.addEventListener('resize',documentHeight)
-documentHeight()
